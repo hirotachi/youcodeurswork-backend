185f62dd5f1582ace57e635071d44fec08b7f100
Said Oudouane <saidoudouane@gmail.com>
2022-05-04 22:45:25 +0000

    hide email when user is not the requester

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,7 +16,7 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'email' => $this->email,
+            'email' => $this->when(auth()->id() === $this->id, $this->email),
             "site" => $this->site,
             "avatar" => $this->avatar,
             "description" => $this->description,
