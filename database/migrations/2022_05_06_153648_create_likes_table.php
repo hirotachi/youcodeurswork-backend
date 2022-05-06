5c218c74dcb82a3e0ab7c5ea4af9da55cb0e4363
Said Oudouane <saidoudouane@gmail.com>
2022-05-06 15:53:51 +0000

    recreate likes table with cascade deletion constraints

--- database/migrations/2022_05_06_153648_create_likes_table.php
+++ database/migrations/2022_05_06_153648_create_likes_table.php
@@ -8,10 +8,11 @@ class CreateLikesTable extends Migration
 {
     public function up()
     {
+        Schema::dropIfExists('likes');
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('project_id')->constrained();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
